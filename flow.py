8a9938a7d1c8511b7255717bf97a1a7435fa93d6
sugarmse <sarthak.sugar@gmail.com>
2025-12-20 11:22:40 +0545

    exercise 4 updated

--- flow.py
+++ flow.py
@@ -25,12 +25,15 @@
 # print(hypotenuse_calculator(length = 5, breadth = 4))
 
 #Exercise 4
-def shouter_function(output_string, repetition_number = 2):
+def shouter_function(output_string="Hello", repetition_number = 1):
     print(list(range(repetition_number)))
     if repetition_number < 10:
         for i in range(repetition_number):
             print(output_string.upper())
     else:
         print("You are too loud!")
+    return "Done!"
 
-shouter_function("Hello World",9)
+#call the function
+status = shouter_function("Hello World",3)
+print(status)
